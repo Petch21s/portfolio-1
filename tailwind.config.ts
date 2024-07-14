c0156c1b9dad3a9c26bb25554f72af8ad1f065f7
Rudolph <30910621+Rudolphredns@users.noreply.github.com>
2024-07-14 16:07:20 +0700

    Bigupdate lates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      backdropBlur: {
+        xs: '2px',
+        sm: '4px',
+        md: '8px',
+        lg: '16px',
+        xl: '24px',
+        '2xl': '40px',
       },
+    },
+  },
+  variants: {
+    extend: {
+      backdropBlur: ['responsive'],
     },
   },
   plugins: [],
 };
+
 export default config;
