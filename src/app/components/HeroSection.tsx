b40aad2e56784c51a5b302b14a4653e7de616269
Rudolph <30910621+Rudolphredns@users.noreply.github.com>
2024-07-17 11:33:46 +0700

    petch

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -14,7 +14,7 @@ const Herosection = () => {
         <div className="col-span-7 place-self-start justify-items-end translate-y-[50px]">
           <h1 className="text-white text-[100px] mt-[240px] ml-[300px] font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-500">
-              Hello, I'm{" "}
+              Hello, I&apos;m{" "}
             </span>
             <div className="py-5 text-white">
               <TypeAnimation 
